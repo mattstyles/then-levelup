ae3b9e543044bf483892ae1e0c8234173480693d
Matt Styles <matt@veryfizzyjelly.com>
2016-03-16 13:46:05 +0000

    add tape

--- test.js
+++ test.js
@@ -1,15 +1,35 @@
 'use strict';
 
-var assert = require('assert');
-var db = require('./')(require('levelup')({
+var tape = require('tape');
+
+var root = require('levelup')({
   db: require('memdown'),
   encoding: 'json'
-}));
-
-db.put('foo', 'bar').then(function () {
-  return db.get('foo');
-}).then(function (value) {
-  assert(value === 'bar');
-}).done(function () {
-  console.log('tests passed');
 });
+var then = require('./');
+
+var db = then( root );
+var sub = then( require( 'level-sublevel' )( root ).sublevel('sub') )
+
+
+tape( 'It wraps the levelup api with promises', function (t) {
+  t.plan( 1 )
+  db.put('foo', 'bar')
+    .then(function () {
+      return db.get('foo');
+    })
+    .then(function (val) {
+      t.ok(val === 'bar', 'sets and gets ok');
+    })
+})
+
+tape( 'It wraps a sublevelled instance api with promises', function (t) {
+  t.plan( 1 )
+  sub.put('foo', 'bar')
+    .then(function () {
+      return sub.get('foo');
+    })
+    .then(function (val) {
+      t.ok(val === 'bar', 'sets and gets ok from sublevel');
+    })
+})
